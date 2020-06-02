2f9bbfe3d54468e68e9d5bb6ac47317131a6333f
Jeremy <jeremyVDLRC@gmail.com>
2020-06-02 22:51:37 +1000

    add reorganise code to be easier to read

--- script_new.js
+++ script_new.js
@@ -1,54 +1,42 @@
 // Assignment Code
 let generateBtn = document.querySelector('#generate');
 let passwordText = document.querySelector('#password');
+
+
+// Add event listener to generate button
+
+generateBtn.addEventListener('click', writePassword);
+
 // Write password to the #password input
+
 function writePassword() {
     userPrompt();
     let password = generatePassword();
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-
-//1 Prompt User:
-// const resultEl = document.getElementById('password');
-// const lengthEl = document.getElementById('length');
-// const uppercaseEl = document.getElementById('uppercase');
-// const lowercaseEl = document.getElementById('lowercase');
-// const numbersEl = document.getElementById('numbers');
-// const symbolsEl = document.getElementById('symbols');
-// const generateEl = document.getElementById('generate');
-
-
-const randomFunc = {
-    lower: getLowercase,
-    upper: getUppercase,
-    number: getRandomNumber,
-    symbol: getRandomSymbol
-};
-
+//1.1 prompt("Please Select length of password between 8 -128 characters")
+//1.2 confirm("lowercase characters?")
+//1.3 confirm("uppercase characters?")
+//1.4 confirm("numbers?")
+//1.5 confirm("symbols?")
 
 
 function userPrompt() {
-
-
-    //1.1 prompt("Please Select length of password between 8 -128 characters")
     let userLength = Number(prompt("Please Select length of password between 8 -128 characters"));
     console.log(userLength);
-    //1.2 confirm("lowercase characters?")
+
     let userLowercase = confirm("lowercase characters?");
     console.log('Use Lowercase?: ' + userLowercase);
 
-    //1.3 confirm("uppercase characters?")
+
     let userUppercase = confirm("uppercase characters?");
     console.log('Use Uppercase?: ' + userUppercase);
 
-    //1.4 confirm("numbers?")
+
     let userNumber = confirm("numbers?")
     console.log('Use Numbers?: ' + userNumber);
 
-    //1.5 confirm("symbols?")
+
     let userSymbols = confirm("symbols?");
     console.log('Use Symbols?: ' + userNumber);
 
@@ -62,18 +50,24 @@ function userPrompt() {
 
     console.log(userLength, userLowercase, userUppercase, userNumber, userSymbols);
 
-    passwordText.value = generatePassword(lowercaseEl, uppercaseEl, numbersEl, symbolsEl, lengthEl);
-
+    passwordText.textContent = generatePassword(lowercaseEl, uppercaseEl, numbersEl, symbolsEl, lengthEl);
 
 
     // let userInputs = [userLength, userLowercase, userUppercase, userNumber, userSymbols];
     // console.log(userInputs);
     // return userInputs;
 
+    return userLength;
 }
 
+const randomFunc = {
+    lower: getLowercase,
+    upper: getUppercase,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
 
-function generatePassword(lower, upper, number, symbol, length) {
+function generatePassword(lower, upper, number, symbol, lengthEl) {
     //1. create password variable
 
     let generatedPassword = '';
@@ -92,7 +86,7 @@ function generatePassword(lower, upper, number, symbol, length) {
         return "";
     }
 
-    for (let i = 0; i < length; i += typesCount) {
+    for (let i = 0; i < lengthEl; i += typesCount) {
         typesArr.forEach(types => {
             const funcName = Object.keys(types)[0];
             console.log('funcName: ', funcName);
@@ -100,41 +94,15 @@ function generatePassword(lower, upper, number, symbol, length) {
             generatedPassword += randomFunc[funcName]();
         });
 
-        console.log(generatedPassword);
+        console.log(generatedPassword.slice(0, lengthEl));
 
-        const finalPassword = generatedPassword;
-        return finalPassword;
+        return generatedPassword;
 
     }
 
 
     //4. Add final password back to variable and return
 }
-
-
-
-
-
-//3 Filter new array to only 'True' values
-// eg.   userValid = [16,False, True,True,True];
-
-
-//4 Write loop that runs the following per loop:
-// 4.1 Randomly selects between 'True' User Selected properties
-// 4.2 Trigger function to randomly select character for property
-// 4.3 Join generated character to new passoword string
-// 4.4 Repeat till loopends
-
-
-
-
-
-
-
-
-
-
-
 
 
 
