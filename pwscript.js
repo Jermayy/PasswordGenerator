81bdcc442eb2b41f0268d486e9ada8aed36ff045
Jeremy <jeremyVDLRC@gmail.com>
2020-06-03 23:53:42 +1000

    created loop function to generate new password as per user input parameters and linked new .js file to index

--- pwscript.js
+++ pwscript.js
@@ -16,38 +16,6 @@ generateBtn.addEventListener('click', writePassword);
 
 // Create series of options that allow user to generate password when press 'Generate Password Button'
 
-function generatePassword() {
-    console.log("I work!");
-
-    const charLength = Number(prompt("select password length between 8 - 128 characters"));
-
-    if (charLength >= 8 && charLength <= 128) {
-
-        const LowercaseChar = confirm("Use lowercase characters?");
-        const uppercaseChar = confirm("Use uppercase characters?");
-        const numericalChar = confirm("Use numerical characters?");
-        const specialChar = confirm("Use Special characters?");
-
-
-
-
-
-
-
-    } else {
-        alert("Password length must be between 8 - 128 characters!");
-    };
-
-
-
-
-
-
-
-
-};
-
-
 //Options:
 //Length of Password
 //lowercase
@@ -61,3 +29,89 @@ function generatePassword() {
 //all character types = TRUE
 // RANDOMLY loop through each TRUE character type function and print new character to PasswordText String
 // When nominated password length is reached, return final password and print on html
+
+
+
+function generatePassword() {
+    console.log("I work!");
+
+    let generatedPassword = "";
+
+    const charLength = Number(prompt("select password length between 8 - 128 characters"));
+
+    if (charLength < 8 || charLength > 128) {
+        alert("Password length must be between 8 - 128 characters!");
+    } else if (charLength >= 8 && charLength <= 128) {
+
+        const lowercaseChar = confirm("Use lowercase characters?");
+        const uppercaseChar = confirm("Use uppercase characters?");
+        const numericalChar = confirm("Use numerical characters?");
+        const specialChar = confirm("Use Special characters?");
+        const typesArr = [lowercaseChar, uppercaseChar, numericalChar, specialChar];
+
+        console.log(typesArr);
+
+
+        typesArr.forEach(element => {
+            for (let i = generatedPassword.length; generatedPassword.length < charLength; i++) {
+
+                if (lowercaseChar === true) {
+                    generatedPassword += getLowercase();
+                    console.log(generatedPassword);
+                };
+
+                if (uppercaseChar === true) {
+                    generatedPassword += getUppercase();
+                    console.log(generatedPassword)
+                };
+
+                if (uppercaseChar === true) {
+                    generatedPassword += getRandomNumber();
+                    console.log(generatedPassword);
+                };
+
+                if (specialChar === true) {
+                    generatedPassword += getRandomSymbol();
+                    console.log(generatedPassword);
+
+                };
+
+            }
+        });
+
+        return generatedPassword;
+
+    };
+
+};
+
+
+
+
+
+
+
+
+
+
+
+function getLowercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(getLowercase());
+
+function getUppercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(getUppercase());
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomNumber());
+
+function getRandomSymbol() {
+    const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '=', '/', '.'];
+    return special[Math.floor(Math.random() * special.length)];
+}
+console.log(getRandomSymbol());
